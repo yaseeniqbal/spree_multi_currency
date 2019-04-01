f35b3afda104a8af4ff668127f4356e8ce3bd3c0
Umair <omsolutionpk@gmail.com>
2019-04-01 09:29:13 +0500

    showing dropdown as country

--- app/controllers/spree/currency_controller.rb
+++ app/controllers/spree/currency_controller.rb
@@ -1,10 +1,13 @@
 module Spree
   class CurrencyController < StoreController
     def set
-      @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
+      @country = params[:currency]
+      config = supported_countries.with_indifferent_access[@country]
+      currency_code = config.dig('currency_name')
+      @currency = supported_currencies.find { |currency| currency.iso_code == currency_code }
       # Make sure that we update the current order, so the currency change is reflected.
       current_order.update_attributes!(currency: @currency.iso_code) if current_order
-      session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+      session[:currency] = currency_code if Spree::Config[:allow_currency_change]
       respond_to do |format|
         format.json { render json: !@currency.nil? }
         format.html do
--- lib/spree/core/currency_helpers.rb
+++ lib/spree/core/currency_helpers.rb
@@ -1,11 +1,32 @@
 module Spree
   module CurrencyHelpers
     def self.included(receiver)
-      receiver.send :helper_method, :supported_currencies
+      receiver.send :helper_method, :supported_currencies, :supported_countries,:country_from_currency
     end
 
     def supported_currencies
-      Spree::Config[:supported_currencies].split(',').map { |code| ::Money::Currency.find(code.strip) }
+      Spree::Config[:supported_currencies].split(',').map {|code| ::Money::Currency.find(code.strip)}
+    end
+
+    def supported_countries
+      {
+          uae: {
+              display_label: 'UAE (د.إ)',
+              country_name: 'UAE',
+              currency_name: 'AED',
+              key: 'uae'
+          },
+          aus: {
+              display_label: 'Australia ($)',
+              country_name: 'Australia',
+              currency_name: 'USD',
+              key: 'aus'
+          }
+      }
+    end
+
+    def country_from_currency
+      supported_countries.with_indifferent_access.select{|k,h| h["currency_name"] == current_currency}.first.first
     end
   end
 end
