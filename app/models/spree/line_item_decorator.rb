e58c1cd53a4636536004a98fc93ce629adecd08e
omairazam <omsolutionpk@gmail.com>
2019-11-04 23:27:06 +0500

    eval replaced with prepend.

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,11 +1,14 @@
-# frozen_string_literal: true
-Spree::LineItem.class_eval do
-  def update_price
-    currency_price = Spree::Price.where(
-      currency: order.currency,
-      variant_id: variant_id
-    ).first
+module Spree
+  module LineItemDecorator
+    def self.prepended(base)
+      def update_price
+        currency_price = Spree::Price.where(
+            currency: order.currency,
+            variant_id: variant_id
+        ).first
 
-    self.price = currency_price.price_including_vat_for(tax_zone: tax_zone)
+        self.price = currency_price.price_including_vat_for(tax_zone: tax_zone)
+      end
+    end
   end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,8 @@
-module Spree
-  Product.class_eval do
-    alias_method :amount=, :price=
+module  Spree
+  module ProductDecorator
+    def self.prepended(base)
+      alias_method :amount=, :price=
+    end
+
   end
 end
