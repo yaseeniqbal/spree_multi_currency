33cb7be67856471a0fc353712ba3a91a03536b01
omairazam <omsolutionpk@gmail.com>
2019-11-12 15:55:16 +0500

    quick hack

--- lib/spree/core/controller_helpers/order_decorator.rb
+++ lib/spree/core/controller_helpers/order_decorator.rb
@@ -9,3 +9,28 @@ Spree::Core::ControllerHelpers::Order.class_eval do
     end
   end
 end
+
+
+
+
+# let following code here - it doesn't work but it has to be there for puma to read file or error willl occur during deployment to heroku
+module Spree
+  module Core
+    module ControllerHelpers
+      module OrderDecorator
+        def current_currency
+          # ensure session currency is supported
+          #
+          if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
+            session[:currency]
+          else
+            Spree::Config[:currency]
+          end
+        end
+      end
+    end
+  end
+end
+
+
+::Spree::Core::ControllerHelpers::Order.prepend(::Spree::Core::ControllerHelpers::OrderDecorator)
